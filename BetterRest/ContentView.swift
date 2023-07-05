e7d7472297852029d72fe84aa451fea6ba80dcb5
Akash <akashsheelavant4@gmail.com>
2023-07-05 13:42:01 +0530

    Always showing recommended bedtime.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -12,10 +12,7 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = defaultWakeTime
     @State private var coffeeAmount = 1
-    
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingAlert = false
+    @State private var recommendedTimeToSleep = ""
     
     static var defaultWakeTime: Date {
         var components = DateComponents()
@@ -27,18 +24,20 @@ struct ContentView: View {
     var body: some View {
         NavigationView  {
             Form {
-                Section("When do you want to wake up?") {
-                    DatePicker("Please enter a time",
-                               selection: $wakeUp,
-                               displayedComponents: .hourAndMinute)
-                    .labelsHidden()
+                Section {
+                    Text("Recommended BedTime: \(recommendedTimeToSleep)")
+                        .font(.system(size: 24, weight: .heavy))
                 }
                 
                 Section("Desired amout of sleep") {
                     Stepper("\(sleepAmount.formatted()) hours",
                             value: $sleepAmount,
                             in: 4...12,
-                            step: 0.25)
+                            step: 0.25) {_ in
+                        calculateBedTime()
+                    }
+                    
+                    
                 }
                 
                 Section("Daily coffee intake") {
@@ -46,20 +45,15 @@ struct ContentView: View {
                            selection: $coffeeAmount) {
                         ForEach(1..<21) {
                             Text($0 == 1 ? "1 cup" : "\($0) cups")
+                            
                         }
                     }
-                }
-                
-                .alert(alertTitle, isPresented: $showingAlert) {
-                    Button("OK") {}
-                }   message: {
-                    Text(alertMessage)
+                           .onReceive([self].publisher.first()) { _ in
+                               calculateBedTime()
+                           }
                 }
             }
             .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Calculate", action: calculateBedTime)
-            }
         }
     }
     
@@ -74,14 +68,10 @@ struct ContentView: View {
             let prediction = try model.prediction(wake: Double(hour+minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
             let sleepTime = wakeUp - prediction.actualSleep
             
-            alertTitle = "Your ideal bedtime is..."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            recommendedTimeToSleep = sleepTime.formatted(date: .omitted, time: .shortened)
         } catch {
             // something went wrong!
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
         }
-        showingAlert = true
     }
 }
 
